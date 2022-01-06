f2c4a8904d5a2bf35ffa64ade346c930476ca72d
carlos herrera <carloshv93@gmail.com>
2022-01-05 19:55:16 -0600

    db name change

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
 
 module.exports = {
-  MONGODB_URI: process.env.MONGODB_URI || "mongodb://localhost/tasksdb",
+  MONGODB_URI: process.env.MONGODB_URI || "mongodb://172.17.0.1:27017/todo",
 };
